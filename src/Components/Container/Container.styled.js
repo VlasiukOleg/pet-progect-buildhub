1ea8dfb0c510a7009827840270425663711548f3
Oleg <ikslprint@gmail.com>
2023-10-03 11:59:42 +0300

    change container padding

--- src/Components/Container/Container.styled.js
+++ src/Components/Container/Container.styled.js
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 export const MainContainer = styled.div`
   max-width: 375px;
   margin: 0 auto;
-  padding: 62px 20px 0 20px;
+  padding: 0px 20px 0 20px;
 
   position: relative;
 
   @media screen and (min-width: 768px) {
     max-width: 768px;
-    padding: 84px 32px 0 32px;
+    padding: 0px 32px 0 32px;
   }
   @media screen and (min-width: 1440px) {
     max-width: 1440px;
